f9f2d9e20abc2295537a3f3c476fc4e256157e51
root <root@DESKTOP-6I3RBRC>
2023-06-12 22:42:16 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+ let greeting;
+ if (distance<=400){ 
+  return 'This one is on me!';
+ } else if (distance > 2000 && distance <2500){
+  return 'I will gladly take your thirty bucks.';
+ } else {
+return 'No can do.';
+ }
+ }
+
+ 
+  
+
+
+function ternaryCheckCity(city){
+return city === 'NYC'? "Ok, sounds good." : "No go.";
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(message){
+  switch (message){
+    case 'generous': return "Thank you so much." ;
+    break;
+    case 'not as generous' : return "Thank you.";
+    break;
+    default: return "Bye.";
+    break;
+  }
   // Write your code here!
 }
